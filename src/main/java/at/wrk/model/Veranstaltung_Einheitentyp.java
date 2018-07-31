39ce4e7857a1ac769a96de1ce126deeec2ef2265
Bernd Roth <bernd@example.com>
2018-07-31 21:16:45 +0200

    added some entities, not yet fully implemented

---
+++ src/main/java/at/wrk/model/Veranstaltung_Einheitentyp.java
@@ -0,0 +1,32 @@
+package at.wrk.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Veranstaltung_Einheitentyp {
+
+	@Id
+	private int nummer;
+	private String veranstaltung;
+	private String einheitentyp;
+	
+	public int getNummer() {
+		return nummer;
+	}
+	public void setNummer(int nummer) {
+		this.nummer = nummer;
+	}
+	public String getVeranstaltung() {
+		return veranstaltung;
+	}
+	public void setVeranstaltung(String veranstaltung) {
+		this.veranstaltung = veranstaltung;
+	}
+	public String getEinheitentyp() {
+		return einheitentyp;
+	}
+	public void setEinheitentyp(String einheitentyp) {
+		this.einheitentyp = einheitentyp;
+	}
+}
