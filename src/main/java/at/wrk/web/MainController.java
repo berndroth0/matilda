d6184091a4ca7355a7275696f3011c5c0a72c5ed
Bernd Roth <bernd@example.com>
2018-09-13 21:07:47 +0200

    logout methode im maincontroller hinzugefuegt

--- src/main/java/at/wrk/web/MainController.java
+++ src/main/java/at/wrk/web/MainController.java
@@ -2,12 +2,20 @@ package at.wrk.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
@@ -30,6 +38,15 @@ public class MainController {
     @GetMapping("/login")
     public String login(Model model) {
         return "login";
+    }
+    
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "login";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
     
 
